91d6f8189f4a9d18dbb563865dd1495dd49795ed
SantiagoPR7-Ing <sanpri2003@gmail.com>
2026-02-19 19:51:51 -0500

    changes on tf main

--- iac/terraform-gcp/main.tf
+++ iac/terraform-gcp/main.tf
@@ -18,7 +18,8 @@ resource "google_project_service" "services" {
   for_each = toset([
     "run.googleapis.com",
     "sqladmin.googleapis.com",
-    "artifactregistry.googleapis.com"
+    "artifactregistry.googleapis.com",
+    "iamcredentials.googleapis.com"
   ])
   service            = each.value
   disable_on_destroy = false
